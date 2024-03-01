3daac3b32623075e00f7717bfd26cd828ce25d0c
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-03-01 20:57:56 +0900

    fn : todo submit 이벤트 추가
    submit 이벤트 추가
    새로고침 방지

--- src/components/wrap/HomeComponent.jsx
+++ src/components/wrap/HomeComponent.jsx
@@ -3,16 +3,23 @@ import React from 'react';
 export default function HomeComponent() {
 
     const [ todo, setTodo ] = React.useState("");
+    
     // todo 입력 이벤트
     const onChangeTodo = (e) => {
         setTodo(e.target.value);
     };
+
+    // todo submit 이벤트
+    const onSubmitTodo = (e) => {
+        e.preventDefault();
+    };
+
   return (
     <div id="home">
         <div className="container">
             <div className="gap">
                 <div className="content">
-                    <form id="todoForm">
+                    <form id="todoForm" onSubmit={onSubmitTodo}>
                         <label htmlFor="userTodo">What To Do ?</label>
                         <input type="text" id='userTodo' name='userTodo' autoComplete='off' onChange={onChangeTodo} />
                         <button type="submit">ADD</button>
