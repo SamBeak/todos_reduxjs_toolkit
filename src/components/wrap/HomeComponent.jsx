2e98e69923f356a407b7f18044a1d80e142f0ba6
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-03-01 20:55:48 +0900

    fn : Add todo input event
    HomeComponent에 todo 입력 이벤트 추가

--- src/components/wrap/HomeComponent.jsx
+++ src/components/wrap/HomeComponent.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default function HomeComponent() {
 
+    const [ todo, setTodo ] = React.useState("");
+    // todo 입력 이벤트
+    const onChangeTodo = (e) => {
+        setTodo(e.target.value);
+    };
   return (
     <div id="home">
         <div className="container">
@@ -9,7 +14,7 @@ export default function HomeComponent() {
                 <div className="content">
                     <form id="todoForm">
                         <label htmlFor="userTodo">What To Do ?</label>
-                        <input type="text" id='userTodo' name='userTodo' autoComplete='off' />
+                        <input type="text" id='userTodo' name='userTodo' autoComplete='off' onChange={onChangeTodo} />
                         <button type="submit">ADD</button>
                     </form>
                     <ul className="todo__list"></ul>
